9ca427170e62a8b576f18dfa441b2a73d9701865
Paul Huff <paul.huff@gmail.com>
2013-04-13 23:02:05 -0600

    Make it so that pulling from the database and generating an epub works through datomic.

--- src/steward/services/nws_weather.clj
+++ src/steward/services/nws_weather.clj
@@ -26,12 +26,12 @@
 (defn getWeather [zipped timeLayouts]
   (let [timeLayout (first (xml-> zipped :data (attr= :type "forecast") :parameters :weather (attr :time-layout)))
         weather (xml-> zipped :data (attr= :type "forecast") :parameters :weather :weather-conditions (attr :weather-summary))]
-    (map (fn [date forecast] {:date date :forecast forecast}) (get timeLayouts timeLayout) weather)))
+    (zipmap (get timeLayouts timeLayout) (map (fn [date forecast] {:date date :forecast forecast}) (get timeLayouts timeLayout) weather))))
 
 (defn getTemps [zipped timeLayouts typeString]
   (let [timeLayout (first (xml-> zipped :data (attr= :type "forecast") :parameters :temperature (attr= :type typeString) (attr :time-layout)))
         temps (xml-> zipped :data (attr= :type "forecast") :parameters :temperature (attr= :type typeString) :value text)]
-    (zipmap (get timeLayouts timeLayout) (map (fn [temp] {:temp temp :type typeString}) temps))))
+    (zipmap (get timeLayouts timeLayout) (map (fn [temp] {:temperature temp :type typeString}) temps))))
 
 (defn getMaxTemps [zipped timeLayouts]
   (getTemps zipped timeLayouts "maximum"))
@@ -44,7 +44,7 @@
     true
     false))
 
-(defn storeTempInDb [connection config temp]
+(defn store-temperature-in-db [connection config temperature]
   (let [result (q '[:find ?e
                     :in $ ?name ?lat ?lon ?date
                     :where
@@ -56,53 +56,116 @@
                   (:location-name config)
                   (:latitude config)
                   (:longitude config)
-                  (to-date (:timestamp (key temp)))
+                  (to-date (:timestamp (key temperature)))
                   )
         tempId (if (> (count result) 0) (ffirst result) (d/tempid :db.part/user))]
-    (println (format "Temp: %s Type: %s Date: %s" (:type (val temp)) (:temp (val temp)) (to-date (:timestamp (key temp)))))
     @(d/transact connection [{:db/id tempId :temperature/location-name (:location-name config)
                               :temperature/location-lat (:latitude config)
                               :temperature/location-lon (:longitude config)
-                              :temperature/value (double (read-string (:temp (val temp))))
-                              :temperature/type (if (= (:type (val temp) "maximum")) :temperature.type/max :temperature.type/min)
-                              :temperature/timestamp (to-date (:timestamp (key temp)))
-                              :temperature/period-name (:period-name (key temp))}])
+                              :temperature/value (double (read-string (:temperature (val temperature))))
+                              :temperature/type (if (= (:type (val temperature) "maximum")) :temperature.type/max :temperature.type/min)
+                              :temperature/timestamp (to-date (:timestamp (key temperature)))
+                              :temperature/period-name (:period-name (key temperature))}])
     )
   )
 
-(defn storeWeatherInDb [config]
+
+(defn store-forecast-in-db [connection config forecast]
+  (let [result (q '[:find ?e
+                    :in $ ?name ?lat ?lon ?date
+                    :where
+                    [?e :weather/location-name ?name]
+                    [?e :weather/location-lat ?lat ]
+                    [?e :weather/location-lon ?lon]
+                    [?e :weather/timestamp ?date]]
+                  (db connection)
+                  (:location-name config)
+                  (:latitude config)
+                  (:longitude config)
+                  (to-date (:timestamp (key forecast)))
+                  )
+        tempId (if (> (count result) 0) (ffirst result) (d/tempid :db.part/user))]
+    @(d/transact connection [{:db/id tempId :weather/location-name (:location-name config)
+                              :weather/location-lat (:latitude config)
+                              :weather/location-lon (:longitude config)
+                              :weather/forecast (:forecast (val forecast))
+                              :weather/timestamp (to-date (:timestamp (key forecast)))
+                              :weather/period-name (:period-name (key forecast))}])
+    )
+  )
+
+(defn store-weather-data-in-db [config]
   (let [zipped (zip/xml-zip (xml/parse (format "http://forecast.weather.gov/MapClick.php?lat=%s&lon=%s&unit=0&lg=english&FcstType=dwml" (:latitude config) (:longitude config))))
         timeLayouts (getTimeLayoutMap zipped)
         maxTemps (getMaxTemps zipped timeLayouts)
         minTemps (getMinTemps zipped timeLayouts)
-        weather  (getWeather zipped timeLayouts)]
+        forecasts  (getWeather zipped timeLayouts)]
     (println "About to create database")
     (d/create-database (:datomic-url config))
     (println "About to connect to db")
     (let [conn (d/connect (:datomic-url config))]
-      (println "Nws Weather about to put maxTemps in db")
       (doseq [temp maxTemps]
-        (storeTempInDb conn config temp)
+        (store-temperature-in-db conn config temp)
         )
-      (println "Nws Weather about to put minTemps in db")
       (doseq [temp minTemps]
-        (storeTempInDb conn config temp)
+        (store-temperature-in-db conn config temp)
+        )
+      (doseq [forecast forecasts]
+        (store-forecast-in-db conn config forecast)
         )
       )))
-;; Refactor this function into two parts: one of them stores things in datomic
-;; The other fetches them from datomic
-(defn getForecastMap [latitude longitude]
-  (let [zipped (zip/xml-zip (xml/parse (format "http://forecast.weather.gov/MapClick.php?lat=%s&lon=%s&unit=0&lg=english&FcstType=dwml" latitude longitude)))
-        timeLayouts (getTimeLayoutMap zipped)
-        maxTemps (getMaxTemps zipped timeLayouts)
-        minTemps (getMinTemps zipped timeLayouts)
-        weather  (getWeather zipped timeLayouts)]
 
-    {:temps (merge maxTemps minTemps)
-     :weather weather}
+(defn get-temps-from-db [conn config]
+  (let [result (q '[:find ?e
+                    :in $ ?name ?lat ?lon ?now
+                    :where
+                    [?e :temperature/location-name ?name]
+                    [?e :temperature/location-lat ?lat ]
+                    [?e :temperature/location-lon ?lon]
+                    [?e :temperature/timestamp ?timestamp]
+                    [(<= ?now ?timestamp)]]
+                  (db conn)
+                  (:location-name config)
+                  (:latitude config)
+                  (:longitude config)
+                  (to-date (clj-time/now))
+                  )]
+    (sort-by #(from-date (:temperature/timestamp %)) clj-time/before?
+             (map (fn [id] (-> conn db (d/entity id))) (map first result)))
     )
   )
 
+(defn get-weather-from-db [conn config]
+  (let [result (q '[:find ?e
+                    :in $ ?name ?lat ?lon ?now
+                    :where
+                    [?e :weather/location-name ?name]
+                    [?e :weather/location-lat ?lat ]
+                    [?e :weather/location-lon ?lon]
+                    [?e :weather/timestamp ?timestamp]
+                    [(<= ?now ?timestamp)]]
+                  (db conn)
+                  (:location-name config)
+                  (:latitude config)
+                  (:longitude config)
+                  (to-date (clj-time/now))
+                  )]
+    (sort-by #(from-date (:weather/timestamp %)) clj-time/before?
+             (map (fn [id] (-> conn db (d/entity id))) (map first result)))
+    )
+  )
+
+;; Refactor this function into two parts: one of them stores things in datomic
+;; The other fetches them from datomic
+(defn getForecastMap [config]
+  (d/create-database (:datomic-url config))
+  (let [conn (d/connect (:datomic-url config))
+        temps (get-temps-from-db conn config)
+        weather (get-weather-from-db conn config)]
+    {:temps (zipmap (map #(:temperature/timestamp %) temps) temps)
+     :weather weather}
+    )
+  )
 
 (defn translateTempType [typeString]
   (if (= typeString "maximum")
@@ -118,22 +181,22 @@
     (doseq [item weather]
       (print (:period-name (:date item)) "\n")
       (print (str "\t" (:forecast item)) "\n")
-      (print (str "\t" (translateTempType (:type (get temps (:date item)))) ": " (:temp (get temps (:date item)))) "\n")
+      (print (str "\t" (translateTempType (:temperature/type (get temps (:weather/timestamp item)))) ": " (:temperature/value (get temps (:weather/timestamp item)))) "\n")
     )))
 
 (defn getTableHeader [forecastMap]
-  (str "<tr>" (apply str (map (fn [item] (str "<th>" (:period-name (:date item)) "</th>")) (:weather forecastMap))) "</tr>"))
+  (str "<tr>" (apply str (map (fn [item] (str "<th>" (:weather/period-name item) "</th>")) (:weather forecastMap))) "</tr>"))
 
 (defn getTableForecastRows [forecastMap]
   (let [temps (:temps forecastMap)]
-    (apply str (map (fn [item] (str "<tr><td>" (:period-name (:date item)) "</td>"
-                                                "<td>" (:forecast item) "</td>"
-                                                "<td>" (translateTempType (:type (get temps (:date item)))) ": " (:temp (get temps (:date item))) "</td></tr>"))
+    (apply str (map (fn [item] (str "<tr><td>" (:weather/period-name item) "</td>"
+                                                "<td>" (:weather/forecast item) "</td>"
+                                                "<td>" (translateTempType (:temperature/type (get temps (:weather/timestamp item)))) ": " (:temperature/value (get temps (:weather/timestamp item))) "</td></tr>"))
                                   (:weather forecastMap)))))
 
 (defn getTableTempRow [forecastMap]
   (let [temps (:temps forecastMap)]
-    (str "<tr>" (apply str (map (fn [item] (str "<td>" (translateTempType (:type (get temps (:date item)))) ": " (:temp (get temps (:date item))) "</td>")) (:weather forecastMap))) "</tr>")))
+    (str "<tr>" (apply str (map (fn [item] (str "<td>" (translateTempType (:temperature/value (get temps (:weather/timestamp item)))) ": " (:temperature/value (get temps (:weather/timestamp item))) "</td>")) (:weather forecastMap))) "</tr>")))
 
 (defn generateDocumentFromMap [forecastMap locationName]
   (str (format "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
@@ -149,7 +212,7 @@
 </html>"))
 
 (defn get-epub-item [config]
-  {:title (format "Weather for %s" (:location-name config)) :content (generateDocumentFromMap (getForecastMap (:latitude config) (:longitude config)) (:weatherLocationName config))})
+  {:title (format "Weather for %s" (:location-name config)) :content (generateDocumentFromMap (getForecastMap config) (:weatherLocationName config))})
 
 
 (defn initialize-service [config]
@@ -160,6 +223,6 @@
   )
 
 (defn collect-data [config]
-  (println "Nws Weather about to storeWeatherInDb")
-  (storeWeatherInDb config)
+  (println "Nws Weather about to store-weather-data-in-db")
+  (store-weather-data-in-db config)
   )
